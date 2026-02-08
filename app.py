bc7b00d6bee9bbd2e166d032dfca95dba30b2ed0
Simon Eppig <simon.eppig@gmail.com>
2026-02-08 11:02:18 +0800

    Added main page

--- app.py
+++ app.py
@@ -6,11 +6,18 @@ app=Flask(__name__)
 def index():
     return redirect(url_for("login"), 302)
 
-@app.route("/login", methods=["GET", "POST"])
+@app.route("/login", methods=["GET"])
 def login():
     return render_template("login.html")
 
+@app.route("/main", methods=["GET", "POST"])
+def main():
+    name = request.form.get("name")
+    stock = request.form.get("stock")
+    interest = request.form.get("interest")
 
+
+    return render_template("main.html", name=name, stock=stock, interest=interest)
 
 if __name__ == "__main__":
     app.run(debug=True)
