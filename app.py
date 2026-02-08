cfc9152740f9630c4f717b57a8777222e0580c28
Simon Eppig <simon.eppig@gmail.com>
2026-02-08 11:51:27 +0800

    Added AI functionality and basic financial information

--- app.py
+++ app.py
@@ -1,4 +1,24 @@
 from flask import Flask, request, render_template, jsonify, redirect, url_for
+from google import genai
+from dotenv import load_dotenv
+import os
+import yfinance as yf
+
+load_dotenv()
+
+
+def get_stock_price(symbol):
+    ticker = yf.Ticker(symbol)
+
+    current_price = ticker.fast_info['last_price']
+    currency = ticker.fast_info['currency']
+
+    return {
+        "symbol": symbol.upper(),
+        "price": current_price,
+        "currency": currency
+    }
+
 
 app=Flask(__name__)
 
@@ -16,8 +36,33 @@ def main():
     stock = request.form.get("stock")
     interest = request.form.get("interest")
 
+    api_key = os.getenv("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
 
-    return render_template("main.html", name=name, stock=stock, interest=interest)
+    industry = client.models.generate_content(
+        model="gemma-3-27b-it",
+        contents=f"Please return the industry related to the following company: {stock} Only return the industry name, nothing else. If the company does not exist, return 'not found'."
+    ).candidates[0].content.parts[0].text
+
+    if industry.strip() == "not found":
+        return "Company not found."
+
+    ticker = client.models.generate_content(
+        model="gemma-3-27b-it",
+        contents=f"Please return the correct Yahoo finance symbol/ticker for the following company: {stock} Remember to return the correct suffix, e.g. .SI for Singapore, .DE for Germany, etc. (no suffix for US). Only return the symbol, nothing else. If the company does not exist or is not publicly listed, return 'not found'."
+    ).candidates[0].content.parts[0].text
+
+    if ticker.strip() == "not found":
+        return "Company is not publicly listed."
+
+    try:
+        stock_price = get_stock_price(ticker)
+    except:
+        stock_price = "not found"
+        return "Financial information could not be extracted. Please try again later."
+
+
+    return render_template("main.html", name=name, stock=stock, interest=interest, industry=industry, price=stock_price)
 
 if __name__ == "__main__":
     app.run(debug=True)
